cf03ab9463d19e792d33f62b1802b10820af6028
J Jones <jlssmmns93@gmail.com>
2018-11-29 09:21:05 -0500

    Add project files.

---
+++ Plant.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Plant.WebMVC.Startup))]
+namespace Plant.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
