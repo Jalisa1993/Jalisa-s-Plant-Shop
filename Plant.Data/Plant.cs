1ec6dd28f50f6c375caa402e074046e51723e210
J Jones <jlssmmns93@gmail.com>
2018-11-29 12:22:13 -0500

    test

--- Plant.Data/Plant.cs
+++ Plant.Data/Plant.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Plant.Data
 {
+    public enum PlantType
+    {
+        Annual,
+        Evergreen,
+        Perennials,
+        Biennial,
+    }
+
     class Plant
     {
+        [Key]
+        public int PlantId { get; set; }
+        [Required]
+        public string PlantName { get; set; }
+        [Required]
+        public  PlantType TypeOfPlant { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+        [Required]
+        public Guid OwnerId { get; set; }
     }
 }
